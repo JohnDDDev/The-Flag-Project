400ff50f98a5529a7f7c032105a4a92b2cafbee8
jbt <animesitrin@gmail.com>
2026-08-13 11:22:45 +0300

    base V1.2

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import pygame
+import consts
+import Screen
+import pygame
 
+state = {
+    'player_x' : 0,
+    'player_y' : 0,
+    'player_state' : 'healthy',
+    'game_state' : 'running'
+}
 
-def create_matrix():
-    pass
+def create_matrix(rows,cols):
+    matrix = [[ '0' for _ in range(consts.MATRIX_COLS)] for _ in range(consts.MATRIX_ROWS) ]
+    return matrix
 
 def player_movement(direction):
     pass
 
+def main():
+    matrix = create_matrix(consts.MATRIX_ROWS, consts.MATRIX_COLS)
 
+    while state['game_state'] == 'running':
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        Screen.draw_game(state)
+
+if __name__ == "__main__":
+    main()
