ef9f1908c3895b9451ef4f83d9b0ea74857aa83f
jbt <linoyhuta@gmail.com>
2026-08-13 11:15:44 +0300

    הוספת פונקציה היוצרת את השחקן

--- Screen.py
+++ Screen.py
@@ -1,7 +1,15 @@
 import pygame
 import consts
 
-screen_height = consts.WINDOW_HEIGHT
+screen= pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
+def create_player(player_image):
+    player_image=pygame.image.load(player_image)
+    sized_player_image = pygame.transform.scale(player_image, (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+    player_image_box= pygame.Surface((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT*2),)
+    player_image_box.blit(sized_player_image, (0, 0))
+    print(player_image_box)
+
 
 
 def change_size():
