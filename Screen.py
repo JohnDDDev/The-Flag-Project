a1414c00d19c1cc143b180faadc909675538e78e
jbt <linoyhuta@gmail.com>
2026-08-13 11:52:58 +0300

    v.3

--- Screen.py
+++ Screen.py
@@ -1,21 +1,39 @@
 import pygame
 import consts
 
-screen= pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+def create_screen():
+    global screen
+    pygame.init()
+    screen= pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+    pygame.display.set_caption("the game ")
+    return screen
 
-def create_player(player_image):
-    player_image=pygame.image.load(player_image)
-    sized_player_image = pygame.transform.scale(player_image, (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-    player_image_box= pygame.Surface((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT*2),)
-    player_image_box.blit(sized_player_image, (0, 0))
-    print(player_image_box)
+def drew_background(screen):
+    screen.fill(consts.BACKGROUND_COLOR)
+
+def drew_grid(screen):
+    for row in range(consts.WINDOW_HEIGHT):
+        for col in range(consts.WINDOW_WIDTH):
+            x = col * consts.TILE_SIZE
+            y = row * consts.TILE_SIZE
+            rect = pygame.Rect(x, y, consts.TILE_SIZE, consts.TILE_SIZE)
+            pygame.draw.rect(screen, consts.BACKGROUND_COLOR, rect)
+
+
+# def create_player(player_image):
+#     player_image=pygame.image.load(player_image)
+#     sized_player_image = pygame.transform.scale(player_image, (2,6))
+#     player_image_box= pygame.Surface((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT*2),)
+#     player_image_box.blit(sized_player_image, (0, 0))
+#     print(player_image_box)
+#     return player_image_box
 
 
 
 def change_size():
     pass
 
-#הפונקציה הראשית של המשחק על הלוח(הציור של המשחק)
-def draw_game():
-    pass
-
+# #הפונקציה הראשית של המשחק על הלוח(הציור של המשחק)
+# def draw_game(state):
+#     create_player(consts.PLAYER_IMAGE)
+#     pygame.display.flip()
